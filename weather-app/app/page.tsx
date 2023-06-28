0ddc5f7193c9adc1f9bc47b1d28238b35994859e
hungmike <maingochungnuce@gmail.com>
2023-06-28 16:37:43 +0700

    complete function of display the list of provinces

--- weather-app/app/page.tsx
+++ weather-app/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { WeatherDetails } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <h1>Weather app</h1>
-    </main>
-  )
+     
+      <main className="weather-app">
+        <WeatherDetails />
+      </main>
+  );
 }
